f28fc434f5050b1496008f73ab161f3185c186db
AllahSaucisse <sidouneurone@gmail.com>
2017-05-19 15:11:49 +0200

    Commentaire chancla

--- Ezio.java
+++ Ezio.java
@@ -83,32 +83,6 @@ public class Ezio extends Assassin{
         return att;
         
     }
-      /**
-    *Cette methode decrit l'attaque speciale du personnage. 
-    *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
-    *@return att modifie l'etat attaque du joueur pour poursuivre le combat.
-    */
-    public boolean aconite(Personnage J2){
-        
-        
-        boolean att = false;
-        
-        if( this.mana >= 50){
-            int diminution =  (int) J2.force * 15/100; //calcul de la diminution de force de J2
-            J2.force = J2.force - diminution; //calcul de la nouvelle force de J2
-            int degat = 50; //inflige les degats à J2
-            System.out.println( this.nom + " s'infiltre dans le camp ennemie et empoisonne l'eau de l'adversaire. " + J2.nom + " est afaiblie et voit sa defense diminuer de 15% et est empoisonne!! (" + degat + " degats pendant 3 tours)");
-            this.poison = 3; //établit le nombre de tour d'empoisonnement
-            this.mana -= 50; //cout en mana
-            att = true; //modifie l'etat d'attaque et sors de la boucle du choix d'attaque
-        }else{
-            System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
-            System.out.println("Rechoisi ton attaque !! ");
-            att = false; //renvoie au choix de l'attaque
-        }
-        
-        return att;
-    }
-        
+
     
 }
