85eab74d51f531170b371129aae1bc570a3b74a5
AllahSaucisse <sidouneurone@gmail.com>
2017-05-19 14:30:41 +0200

    Update Achille.java

--- Achille.java
+++ Achille.java
@@ -6,19 +6,22 @@ public class Achille extends Guerrier{
         
         super("Achille", 250, 100 , 350, 230, 110, 0, 20); //nom, vie, mana, defense, force, vitesse, magie, recupération
     }
-    
-     public void attaquer(Personnage J2){
-        
+      /** 
+     *La methode attaquer permet au joueur de selectionner son attaque.
+     *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+     */
+     public void attaquer(Personnage J2){ 
         Scanner sc = new Scanner(System.in);
         boolean attok = false;
         
-        System.out.println(this.nom + " Attaque. Choisi ton attaque :");
+        System.out.println(this.nom + " Attaque. Choisi ton attaque :"); 
         System.out.println("1-Talonnette     2-Boularmure     3-Tibo Yinshape    4- Pichenette   5-This is sparta   6-Passer son tour" );
         
-        while(attok == false){
+        while(attok == false){ /**Attend que le joueur fasse un choix d'attaque valide (= choisir une attaque + mana suffisant)
+                                  Attribut chaque attaque a un nombre*/
             int choixAttaque = sc.nextInt();
             
-            switch (choixAttaque){
+            switch (choixAttaque){ 
                 case 1:
                     attok = talonnette(J2);
                 break;
@@ -40,28 +43,32 @@ public class Achille extends Guerrier{
                 break;
                 default:
                     attok = false;
-                    System.out.println("attaque saisie incorrecte. Rechoisi ton attaque !! ");
+                    System.out.println("attaque saisie incorrecte. Rechoisi ton attaque !! "); 
                 break;
             }
         }
         
     }
-    
-    public boolean talonnette(Personnage J2){
+    /**
+    *Cette methode decrit l'attaque speciale du personnage. 
+    *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+    *@return att modifie l'etat attaque du joueur pour poursuivre le combat.
+    */
+    public boolean talonnette(Personnage J2){ 
         boolean att = false;
         
-        if( this.mana >= 50){
+        if( this.mana >= 50){ //condition de mana
             
              int degat = 250;
-            J2.vie = J2.vie - degat;
+            J2.vie = J2.vie - degat; //inflige les degats à J2
             System.out.println(this.nom + " se propulse sur l'ennemi par la force se son talon !! La défense " + J2.nom + " est sans effet !! Il fait " + degat + " a "+ J2.nom);
             
-            this.mana  = this.mana - 50;
-            att =true;
+            this.mana  = this.mana - 50; //cout en mana de l'attaque
+            att =true; //modifie l'etat d'attaque et sors de la boucle du choix d'attaque
         }else{
             System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
             System.out.println("Rechoisi ton attaque !! ");
-            att = false;
+            att = false; //renvoie au choix de l'attaque
         }
         
         return att;
