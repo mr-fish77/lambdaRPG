d91e6a0a5d1d4244264a6bcca502af2685f78f1d
AllahSaucisse <sidouneurone@gmail.com>
2017-05-19 15:06:07 +0200

    Commentaires

--- Isis.java
+++ Isis.java
@@ -6,7 +6,10 @@ public class Isis extends Necromancien{
         
         super("Isis", 500, 100 , 50, 100, 200, 450, 20);     //nom, vie, mana, defense, force, vitesse, magie, recupération
     }
-    
+          /** 
+     *La methode attaquer permet au joueur de selectionner son attaque.
+     *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+     */
      public void attaquer(Personnage J2){
         
         
@@ -16,7 +19,8 @@ public class Isis extends Necromancien{
         System.out.println(this.nom + " Attaque. Choisi ton attaque:");
         System.out.println("1-Sacrifice   2-Malediction    3-Lance de macchabes   4- Caresse de la mort   5-Noces funebres     6-Passer son tour" );
         
-        while(attok == false){
+        while(attok == false){ /**Attend que le joueur fasse un choix d'attaque valide (= choisir une attaque + mana suffisant)
+                                  Attribut chaque attaque a un nombre*/
             int choixAttaque = sc.nextInt();
             
             switch (choixAttaque){
@@ -47,21 +51,25 @@ public class Isis extends Necromancien{
         }
         
     }
-       
+        /**
+    *Cette methode decrit l'attaque speciale du personnage. 
+    *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+    *@return att modifie l'etat attaque du joueur pour poursuivre le combat.
+    */
     public boolean sacrifice(Personnage J2){
         boolean att = false;
         
-        if( this.mana >= 50){
+        if( this.mana >= 50){ //condition de mana
             
             int degat = 250;
-            J2.vie = J2.vie - degat;
+            J2.vie = J2.vie - degat; //inflige les dégats de l'attaque
             System.out.println(this.nom + " 72 vierges debarquent des enfers pour attaquer l'adversaire " +J2.nom+ " subit 250 de degats");
-            this.mana  = this.mana - 50;
-            att =true;
+            this.mana  = this.mana - 50; //cout en mana de l'attaque 
+            att =true; //modifie l'etat d'attaque et sors de la boucle du choix d'attaque
         }else{
             System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
             System.out.println("Rechoisi ton attaque !! ");
-            att = false;
+            att = false; //renvoie au choix de l'attaque
         }
         
         return att;
