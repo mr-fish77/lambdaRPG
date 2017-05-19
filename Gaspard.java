85b42aea83f95ac792640601fc5ec3b426cffea2
AllahSaucisse <sidouneurone@gmail.com>
2017-05-19 14:53:31 +0200

    Update Gaspard.java

--- Gaspard.java
+++ Gaspard.java
@@ -6,7 +6,10 @@ public class Gaspard extends Mage{
         
         super("Gaspard", 600, 100 , 50, 90, 180, 300, 20); //nom, vie, mana, defense, force, vitesse, magie, recupération
     }
-    
+    /** 
+     *La methode attaquer permet au joueur de selectionner son attaque.
+     *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+     */
      public void attaquer(Personnage J2){
         
         boolean attok = false;
@@ -15,7 +18,8 @@ public class Gaspard extends Mage{
         System.out.println(this.nom + " Attaque. Choisi ton attaque:");
         System.out.println("1-Encens       2-La Main De Midas     3-Yami no mahO    4- Flammeche    5-Passer son tour");
         
-        while(attok == false){
+        while(attok == false){ /**Attend que le joueur fasse un choix d'attaque valide (= choisir une attaque + mana suffisant)
+                                  Attribut chaque attaque a un nombre*/
             int choixAttaque = sc.nextInt();
             
             switch (choixAttaque){
@@ -43,49 +47,29 @@ public class Gaspard extends Mage{
         }
         
     }
-    
+        /**
+    *Cette methode decrit l'attaque speciale du personnage. 
+    *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+    *@return att modifie l'etat attaque du joueur pour poursuivre le combat.
+    */
     public boolean encens(Personnage J2){
         
         boolean att = false;
         
-        if( this.mana >= 50){
-            int diminution =  (int) J2.force * 15/100;
-            J2.force = J2.force - diminution;
+        if( this.mana >= 50){ //condition de mana
+            int diminution =  (int) J2.force * 15/100; //calcul de la diminution de force
+            J2.force = J2.force - diminution; //calcul la nouvelle force
             System.out.println( this.nom + " lance un nuage d'encens qui affaiblit " + J2.nom + ". La force de " +J2.nom + "diminue de 15% ");
-            this.mana -= 50;
-            att = true;
+            this.mana -= 50; //cout en mana de l'attaque
+            att = true; //modifie l'etat d'attaque et sors de la boucle du choix d'attaque
         }else{
             System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
             System.out.println("Rechoisi ton attaque !! ");
-            att = false;
+            att = false; //renvoie au choix de l'attaque
         }
         
         return att;
     }
-    
-    
-    public boolean laMainDeMidas(){
         
-        boolean att = false;
-        
-        if( this.mana >= 40){
-        
-            int diminution =  (int) this.vitesse * 10/100;
-            this.vitesse = this.vitesse - diminution;
-            int augmentation =  (int) this.defense * 20/100;
-            this.defense = this.defense + augmentation;  
-            System.out.println(this.nom + " change sa cape en armure d'or. Defense +20%. Vitesse -10%" );
-            this.mana -= 40;
-            att = false;
-        }else{
-            System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
-            System.out.println("Rechoisi ton attaque !! ");
-            att = false;
-        }
-        
-        return att;
-
-    }
-    
     
 }
