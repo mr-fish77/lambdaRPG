a6dd65ce49e6c90f2f4d925bb8e1236110637b54
AllahSaucisse <sidouneurone@gmail.com>
2017-05-19 14:48:31 +0200

    Commentaires

--- Ezio.java
+++ Ezio.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
 
 public class Ezio extends Assassin{
-    int poison;
+    int poison; //variable de stockage du nombre de tour d'empoisonnement
     
     public Ezio(){
         
         super("Ezio Auditore da Firenze", 280, 100 , 650, 110, 230, 0, 20);     //nom, vie, mana, defense, force, vitesse, magie, recupération
         int poison =0;
     }
-    
+     /** 
+     *La methode attaquer permet au joueur de selectionner son attaque.
+     *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+     */
      public void attaquer(Personnage J2){
-        
         
         boolean attok = false;
         Scanner sc = new Scanner(System.in);
-        
-        if(poison != 0){
-            J2.vie = J2.vie - 50;
+        //Boucle d'empoisonnement
+        if(poison != 0){ //condition d'empoisonnement
+            J2.vie = J2.vie - 50; //inflige les dégats de l'attaque
             System.out.println(J2.nom + " est empoisonne, il subit 50 degats !! ");
             System.out.println("");
-            poison --;
+            poison --; //décrémente le nombre de tour d'empoisonnement
         }
 
         System.out.println(this.nom + " Attaque. Choisi ton attaque:");
         System.out.println("1-Lames secretes    2-Aconite     3-Burqa    4- Dague vengeresse   5-Execution     6-Passer son tour" );
         
-        while(attok == false){
+        while(attok == false){/**Attend que le joueur fasse un choix d'attaque valide (= choisir une attaque + mana suffisant)
+                                  Attribut chaque attaque a un nombre*/
             int choixAttaque = sc.nextInt();
             
             switch (choixAttaque){
@@ -56,45 +59,52 @@ public class Ezio extends Assassin{
         }
         
     }
-       
+        /**
+    *Cette methode decrit l'attaque speciale du personnage. 
+    *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+    *@return att modifie l'etat attaque du joueur pour poursuivre le combat.
+    */
     public boolean lamesecrete(Personnage J2){
         boolean att = false;
         
-        if( this.mana >= 50){
+        if( this.mana >= 50){  //condition de mana
             
             int degat = 250;
-            J2.vie = J2.vie - degat;
+            J2.vie = J2.vie - degat; //inflige les degats à J2
             System.out.println(this.nom + " attaque avec des lames cachees dans ses gantelets situes sur ses avant-bras. Il fait " + degat + " degats a " + J2.nom);
-            this.mana  = this.mana - 50;
-            att =true;
+            this.mana  = this.mana - 50; //cout en mana de l'attaque
+            att =true; //modifie l'etat d'attaque et sors de la boucle du choix d'attaque
         }else{
             System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
             System.out.println("Rechoisi ton attaque !! ");
-            att = false;
+            att = false; //renvoie au choix de l'attaque
         }
         
         return att;
         
     }
-    
-     
+      /**
+    *Cette methode decrit l'attaque speciale du personnage. 
+    *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+    *@return att modifie l'etat attaque du joueur pour poursuivre le combat.
+    */
     public boolean aconite(Personnage J2){
         
         
         boolean att = false;
         
         if( this.mana >= 50){
-            int diminution =  (int) J2.force * 15/100;
-            J2.force = J2.force - diminution;
-            int degat = 50;
+            int diminution =  (int) J2.force * 15/100; //calcul de la diminution de force de J2
+            J2.force = J2.force - diminution; //calcul de la nouvelle force de J2
+            int degat = 50; //inflige les degats à J2
             System.out.println( this.nom + " s'infiltre dans le camp ennemie et empoisonne l'eau de l'adversaire. " + J2.nom + " est afaiblie et voit sa defense diminuer de 15% et est empoisonne!! (" + degat + " degats pendant 3 tours)");
-            this.poison = 3;
-            this.mana -= 50;
-            att = true;
+            this.poison = 3; //établit le nombre de tour d'empoisonnement
+            this.mana -= 50; //cout en mana
+            att = true; //modifie l'etat d'attaque et sors de la boucle du choix d'attaque
         }else{
             System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
             System.out.println("Rechoisi ton attaque !! ");
-            att = false;
+            att = false; //renvoie au choix de l'attaque
         }
         
         return att;
