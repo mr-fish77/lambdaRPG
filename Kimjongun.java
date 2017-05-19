d990b2912aa446ce742c1ec9a8066b744bf7c19b
AllahSaucisse <sidouneurone@gmail.com>
2017-05-19 15:16:46 +0200

    Commentaires
    bing bong bing bing bong

--- Kimjongun.java
+++ Kimjongun.java
@@ -7,7 +7,10 @@ public class Kimjongun extends Guerrier{
         super("Kim jong un", 350, 100 , 250, 220, 120, 0, 20);     //nom, vie, mana, defense, force, vitesse, magie, recupération
 
     }
-    
+     /** 
+     *La methode attaquer permet au joueur de selectionner son attaque.
+     *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+     */
      public void attaquer(Personnage J2){
         
         boolean attok = false;
@@ -16,7 +19,8 @@ public class Kimjongun extends Guerrier{
         System.out.println(this.nom + " Attaque. Choisi ton attaque:");
         System.out.println("1-Lance missiles    2-Boularmure     3-Tibo Yinshape    4- Pichenette   5-This is sparta    6-Passer son tour" );
         
-        while(attok == false){
+        while(attok == false){ /**Attend que le joueur fasse un choix d'attaque valide (= choisir une attaque + mana suffisant)
+                                  Attribut chaque attaque a un nombre*/
             int choixAttaque = sc.nextInt();
             
             switch (choixAttaque){
@@ -48,20 +52,25 @@ public class Kimjongun extends Guerrier{
         
     }
        
+    /**
+    *Cette methode decrit l'attaque speciale du personnage. 
+    *@param Personnage J2 prend en parametre le joueur 2 qui est le joueur a attaquer.
+    *@return att modifie l'etat attaque du joueur pour poursuivre le combat.
+    */
     public boolean lanceMissile(Personnage J2){
         boolean att = false;
         
-        if( this.mana >= 50){
+        if( this.mana >= 50){ //condition de mana
             
-            int degat = this.vitesse;
-            J2.vie = J2.vie - degat;
+            int degat = this.vitesse; //détermine les dégats comme égaux à la vitesse du joueur
+            J2.vie = J2.vie - degat; //inflige les dégats de l'attaque
             System.out.println(this.nom + " lance ses missiles sur l'adversaire!! La defense de " + J2.nom + " est sans effet !!   Il fait " + degat + " degats a " + J2.nom);
-            this.mana  = this.mana - 50;
-            att =true;
+            this.mana  = this.mana - 50; //cout en mana de l'attaque
+            att =true;  //modifie l'etat d'attaque et sors de la boucle du choix d'attaque
         }else{
             System.out.println(this.nom + " n'a pas assez de mana pour attaquer" );
             System.out.println("Rechoisi ton attaque !! ");
-            att = false;
+            att = false; //renvoie au choix de l'attaque
         }
         
         return att;
