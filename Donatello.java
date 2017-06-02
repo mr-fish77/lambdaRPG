1942d2101760e58ce03e1906129f39d3b67c940c
Copine59 <adrien59287@gmail.com>
2017-06-02 14:16:34 +0200

    Update Donatello.java

--- Donatello.java
+++ Donatello.java
@@ -19,10 +19,10 @@ public class Donatello extends Assassin{
        
 
         System.out.println(this.nom + " Attaque. Choisi ton attaque:");
-        System.out.println("1-");  
-        System.out.println("2-");
-        System.out.println("3-");
-        System.out.println("4-");
+        System.out.println("1- Attaque faible mais qui diminue la defense de J2");  
+        System.out.println("2- Attaque puissante avec 1 chance sur 2 de causer 2 fois plus de degats ");
+        System.out.println("3- Attaque moyenne ");
+        System.out.println("4- Augmente la vitesse de J1 de 20%");
         System.out.println("5- Attaque moderee mais possibilite de recuperer 100 HP");
         System.out.println("6- Diminue vitesse de J2 de 10%");
         System.out.println("7- Vous recuperez 100 HP");
